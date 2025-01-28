63c55acc03d7d173c9bdf95d96d128e4b9ece849
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-01-28 13:47:45 +0530

    working

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //refer the mybox
+  final _myBox = Hive.box('mybox');
+
+  //write data
+  void writeData() {
+    _myBox.put(1, 'Nadun');
+  }
+
+  //read data
+  void readData() {
+    print(_myBox.get(1));
+  }
+
+  //delete data
+  void deleteData() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +32,14 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: writeData,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue[200], // Set the background color
               ),
               child: const Text("Write"),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: readData,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue[200], // Set the background color
               ),
